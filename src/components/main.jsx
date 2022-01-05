c3092e8f32251ded564b826cd81ae7decbb0f144
Mukesh Gurpude <mukeshgurpude02@gmail.com>
2022-01-06 01:59:25 +0530

    feat: add input styles

--- src/components/main.jsx
+++ src/components/main.jsx
@@ -1,4 +1,15 @@
+import styled from 'styled-components';
+import Input from './input';
+
 export default function Main() {
-  return <main>
-  </main>
+  return <Container>
+    <h3>Inputs</h3>
+    <Input />
+    <Input multiline />
+  </Container>
 }
+
+const Container = styled.main`
+  padding: 1rem;
+  color: #656565;
+`
