be9d341cf454c155b8d5da59972b57d9b0667109
Mukesh Gurpude <mukeshgurpude02@gmail.com>
2022-01-06 19:16:47 +0530

    feat: add icons

--- src/components/main.jsx
+++ src/components/main.jsx
@@ -1,44 +1,54 @@
 import styled from 'styled-components';
 import Input from './input';
 
-function Child ({text, children}) {
+function Child ({text, children, normal}) {
   return <div>
-    <h5>{text}</h5>
+    <Heading normal={normal}>{text}</Heading>
     {children}
   </div>
 }
 
-export function InputContainer ({children: Children, emulate, text, ...props}) {
+export function InputContainer ({emulate, text, ...props}) {
   if (!!emulate) {  // Emulate hover and focus ui
     return <Parent>
-      <Child text={`<Input ${text} />`} children={<Children {...props} />} />
-      <Child text={'&:hover'} children={<Children {...props} hover />} />
-      <Child text={'&:focus'} children={<Children {...props} focus />} />
+      <Child text={`<Input ${text} />`} children={<Input {...props} />} />
+      <Child text={'&:hover'} normal={true} children={<Input {...props} hover />} />
+      <Child text={'&:focus'} normal={true} children={<Input {...props} focus />} />
     </Parent>
   }
   return <>
-    <Child text={`<Input ${text} />`} children={<Children {...props} />} />
+    <Child text={`<Input ${text} />`} children={<Input {...props} />} />
   </>
 }
 
 export default function Main() {
   const helper = 'Some interesting text'
+  const label = 'Labeled input'
   return <Container>
     <h3>Inputs</h3>
-    <InputContainer children={Input} emulate text='' />
-    <InputContainer children={Input} error emulate text='error' />
-    <InputContainer children={Input} disabled text='disabled' />
+    <InputContainer emulate text='' />
+    <InputContainer emulate text={`label="${label}"`} label={label} />
+    <InputContainer error emulate text='error' />
+    <InputContainer disabled text='disabled' />
     <Parent>
-      <InputContainer children={Input} helperText={helper} text={`helperText=${helper}`}/>
-      <InputContainer children={Input} helperText={helper} error text={`helperText=${helper} error`}/>
+      <InputContainer helperText={helper} text={`helperText="${helper}"`}/>
+      <InputContainer helperText={helper} error text={`helperText="${helper}" error`}/>
     </Parent>
-    <InputContainer children={Input} value='Text' text='value="Text"' />
     <Parent>
-      <InputContainer children={Input} size='sm' text='size="sm"' />
-      <InputContainer children={Input} size='md' text='size="md"' />
+      <InputContainer startIcon='phone' text='startIcon="phone"' />
+      <InputContainer endIcon='lock' text='endIcon="lock"' />
     </Parent>
-    <InputContainer children={Input} fullWidth text='fullWidth' />
-    <InputContainer children={Input} multiline text='multiline' />
+    <InputContainer value='Text' text='value="Text"' />
+    <Parent>
+      <InputContainer size='sm' text='size="sm"' />
+      <InputContainer size='md' text='size="md"' />
+    </Parent>
+    <InputContainer fullWidth text='fullWidth' />
+    <InputContainer fullWidth text='error fullWidth startIcon="android"' startIcon='android' error />
+    <Parent>
+    <InputContainer multiline text='multiline' />
+    <InputContainer multiline text='multiline row="4"' row={4} />
+    </Parent>
   </Container>
 }
 
@@ -47,9 +57,16 @@ const Container = styled.main`
   color: #656565;
   width: 100%;
 `
-
 const Parent = styled.div`
   display: flex;
   flex-flow: wrap;
-  gap: 2rem;
+  gap: 3rem;
+`
+const Heading = styled.span`
+  display: inline-block;
+  font-weight: 500;
+  margin-top: 1em;
+  padding: .5em 0;
+  font-family: ubuntu-mono, monspace;
+  ${({normal}) => normal || 'color: #000;'}
 `
