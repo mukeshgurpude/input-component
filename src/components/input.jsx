9cbc3f5d3abb4079fb08055bd83ef7048dce431e
Mukesh Gurpude <mukeshgurpude02@gmail.com>
2022-01-06 17:32:40 +0530

    reorganize css for label coloring

--- src/components/input.jsx
+++ src/components/input.jsx
@@ -14,39 +14,27 @@ export default function Input(props) {
     helperText: props.helperText || "",
     placeholder: props.placeholder || "Placeholder",
   }
-
+  const label = props.label || 'Label'
   const [value, setValue] = useState(props.value || '');
 
-  return <Container>
-    {
-      props.multiline
-      ?
-      <MultiText {...mappedProps} value={value} onChange={(e) => setValue(e.target.value)} />
-      :
-      <Text type='text' {...mappedProps} value={value} onChange={(e) => setValue(e.target.value)} />
-    }
+  return <Container {...mappedProps}>
+    <Label>
+      <span>{label}</span>
+      {
+        props.multiline
+        ?
+        <MultiText {...mappedProps} value={value} onChange={(e) => setValue(e.target.value)} />
+        :
+        <Text type='text' {...mappedProps} value={value} onChange={(e) => setValue(e.target.value)} />
+      }
+    </Label>
   </Container>
 }
 
-const Container = styled.div``
-const genericinput = css`
+const Container = styled.div`
   --border-color: #828282;
   --border-color-hover: #333333;
   --border-color-focus: #2962FF;
-  border: 1.5px solid var(--border-color);
-  border-radius: 3px;
-  padding: 0.5rem;
-  margin-bottom: 1rem;
-  outline: none;
-
-  ${({fullWidth}) => fullWidth && 'width: 100%;'}
-
-  &:hover {
-    border-color: var(--border-color-hover)
-  }
-  &:focus {
-    border-color: var(--border-color-focus)
-  }
   ${({error}) => error && 
     '--border-color: #D84949;--border-color-hover: #333;--border-color-focus: #D32F2F;'}
   ${({disabled}) => disabled &&
@@ -56,6 +44,36 @@ const genericinput = css`
     '--border-color: var(--border-color-hover);'}
   ${({focus}) => focus &&
     '--border-color: var(--border-color-focus);'}
+`
+const Label = styled.label`
+  display: flex;
+  flex-flow: column;
+  row-gap: .2rem;
+  color: var(--border-color);
+`
+const genericinput = css`
+  border: 1.5px solid var(--border-color);
+  border-radius: 3px;
+  padding: .75rem 0.5rem;
+  margin-bottom: 1rem;
+  outline: none;
+
+  ${({size}) => {
+    switch(size) {
+      case 'sm':
+        return 'width: 20ch;'
+      case 'md':
+        return 'width: 25ch;'
+    }
+  }}
+  ${({fullWidth}) => fullWidth && 'width: 100%;'}
+
+  &:hover {
+    border-color: var(--border-color-hover)
+  }
+  &:focus {
+    border-color: var(--border-color-focus)
+  }
 `
 const Text = styled.input`
   ${genericinput}
--- src/components/main.jsx
+++ src/components/main.jsx
@@ -1,20 +1,42 @@
 import styled from 'styled-components';
 import Input from './input';
 
-export function InputContainer ({children: Children, ...props}) {
+function Child ({text, children}) {
+  return <div>
+    <h4>{text}</h4>
+    {children}
+  </div>
+}
+
+export function InputContainer ({children: Children, emulate, text, ...props}) {
+  if (!!emulate) {
+    return <Parent>
+      <Child text={`<Input ${text} />`} >
+        <Children {...props} />
+      </Child>
+      <Child text={'&:hover'}>
+        <Children {...props} hover />
+      </Child>
+      <Child text={'&:focus'}>
+        <Children {...props} focus />
+      </Child>
+    </Parent>
+  }
   return <>
-    <Children {...props} />
+    <Child text={`<Input ${text} />`}>
+      <Children {...props} />
+    </Child>
   </>
 }
 
 export default function Main() {
   return <Container>
     <h3>Inputs</h3>
-    <InputContainer children={Input} />
-    <InputContainer children={Input} error />
-    <InputContainer children={Input} disabled />
-    <InputContainer children={Input} fullWidth />
-    <InputContainer children={Input} multiline />
+    <InputContainer children={Input} emulate text='' />
+    <InputContainer children={Input} error emulate text='error' />
+    <InputContainer children={Input} disabled text='disabled' />
+    <InputContainer children={Input} fullWidth text='fullWidth' />
+    <InputContainer children={Input} multiline text='multiline' />
   </Container>
 }
 
@@ -22,4 +44,10 @@ const Container = styled.main`
   padding: 1rem;
   color: #656565;
   width: 100%;
+`
+
+const Parent = styled.div`
+  display: flex;
+  justify-content: space-between;
+  flex-flow: wrap;
 `
